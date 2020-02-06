30978f1c5807a88109b55c0a35656e623a1299d7
zectre <43196730+zectre@users.noreply.github.com>
2020-02-06 23:31:40 +0700

    Add files via upload
    Peta Tutupan Lahan dan Fasilitas Desa Kandangrejo

---
+++ data/FasilitasUmum_5.js
@@ -0,0 +1,26 @@
+var json_FasilitasUmum_5 = {
+"type": "FeatureCollection",
+"name": "FasilitasUmum_5",
+"crs": { "type": "name", "properties": { "name": "urn:ogc:def:crs:OGC:1.3:CRS84" } },
+"features": [
+{ "type": "Feature", "properties": { "q2wHide_Objek": "Sekolah", "NAMA": "SDN 4 Kandangrejo", "Gambar": "<img src=\"https:\/\/i.imgur.com\/rF2kOxe.jpg\" height=\"80\" width=\"100\">" }, "geometry": { "type": "Point", "coordinates": [ 110.829161016467822, -7.022418995888524 ] } },
+{ "type": "Feature", "properties": { "q2wHide_Objek": "Sekolah", "NAMA": "SDN 1 Kandangrejo", "Gambar": "<img src=\"https:\/\/i.imgur.com\/3w9N4X9.jpg\" height=\"80\" width=\"100\">" }, "geometry": { "type": "Point", "coordinates": [ 110.830587784149472, -7.025011521548736 ] } },
+{ "type": "Feature", "properties": { "q2wHide_Objek": "Sekolah", "NAMA": "SDN 3 Kandangrejo", "Gambar": "<img src=\"https:\/\/i.imgur.com\/jfxieCi.jpg\" height=\"80\" width=\"100\">" }, "geometry": { "type": "Point", "coordinates": [ 110.845823733624613, -7.031600619106706 ] } },
+{ "type": "Feature", "properties": { "q2wHide_Objek": "Sekolah", "NAMA": "SDN 2 Kandangrejo", "Gambar": "<img src=\"https:\/\/i.imgur.com\/JUDUdu1.jpg\" height=\"80\" width=\"100\">" }, "geometry": { "type": "Point", "coordinates": [ 110.848964378496319, -7.032958824920548 ] } },
+{ "type": "Feature", "properties": { "q2wHide_Objek": "Kantor Desa", "NAMA": "Balai Desa Kandangrejo", "Gambar": "<img src=\"https:\/\/i.imgur.com\/FBj9m1p.jpg\" height=\"80\" width=\"100\">" }, "geometry": { "type": "Point", "coordinates": [ 110.845007153178713, -7.029875862142696 ] } },
+{ "type": "Feature", "properties": { "q2wHide_Objek": "Masjid", "NAMA": "Masjid Ngeluk", "Gambar": null }, "geometry": { "type": "Point", "coordinates": [ 110.829266295960977, -7.025889237913003 ] } },
+{ "type": "Feature", "properties": { "q2wHide_Objek": "Makam", "NAMA": "Makam", "Gambar": null }, "geometry": { "type": "Point", "coordinates": [ 110.831524342829724, -7.027912566106475 ] } },
+{ "type": "Feature", "properties": { "q2wHide_Objek": "Masjid", "NAMA": "Masjid Sidiqiyah", "Gambar": null }, "geometry": { "type": "Point", "coordinates": [ 110.842885346954873, -7.029390255315759 ] } },
+{ "type": "Feature", "properties": { "q2wHide_Objek": "Makam", "NAMA": "Makam", "Gambar": null }, "geometry": { "type": "Point", "coordinates": [ 110.840575105878074, -7.028974889599638 ] } },
+{ "type": "Feature", "properties": { "q2wHide_Objek": "Makam", "NAMA": "Makam", "Gambar": null }, "geometry": { "type": "Point", "coordinates": [ 110.846925021546653, -7.029807301364152 ] } },
+{ "type": "Feature", "properties": { "q2wHide_Objek": "Masjid", "NAMA": "Makam", "Gambar": null }, "geometry": { "type": "Point", "coordinates": [ 110.85014149177222, -7.033704339226258 ] } },
+{ "type": "Feature", "properties": { "q2wHide_Objek": "Masjid", "NAMA": "Masjid Mutakin Ploso", "Gambar": null }, "geometry": { "type": "Point", "coordinates": [ 110.828126157728164, -7.022100880579412 ] } },
+{ "type": "Feature", "properties": { "q2wHide_Objek": "Masjid", "NAMA": "Mushola Rodhotul Jannah", "Gambar": null }, "geometry": { "type": "Point", "coordinates": [ 110.830769095612098, -7.029121099321782 ] } },
+{ "type": "Feature", "properties": { "q2wHide_Objek": "Masjid", "NAMA": "Masjid Nurul Yaqin", "Gambar": null }, "geometry": { "type": "Point", "coordinates": [ 110.832277220656351, -7.031610158114582 ] } },
+{ "type": "Feature", "properties": { "q2wHide_Objek": "Masjid", "NAMA": "Masjid Jami Baitul Muhaimin", "Gambar": null }, "geometry": { "type": "Point", "coordinates": [ 110.844373731695399, -7.029734539643778 ] } },
+{ "type": "Feature", "properties": { "q2wHide_Objek": "Masjid", "NAMA": "Masjid Agung Ngemplak", "Gambar": null }, "geometry": { "type": "Point", "coordinates": [ 110.856287087623343, -7.038006598998703 ] } },
+{ "type": "Feature", "properties": { "q2wHide_Objek": "Masjid", "NAMA": "Mushola Al Huda", "Gambar": null }, "geometry": { "type": "Point", "coordinates": [ 110.85595936745554, -7.041152628900969 ] } },
+{ "type": "Feature", "properties": { "q2wHide_Objek": "Masjid", "NAMA": "Mushola Nurul Falah", "Gambar": null }, "geometry": { "type": "Point", "coordinates": [ 110.857035348142418, -7.042998057994049 ] } },
+{ "type": "Feature", "properties": { "q2wHide_Objek": "Kesehatan", "NAMA": "Poliklinik Kesehatan Desa", "Gambar": "<img src=\"https:\/\/i.imgur.com\/tmiCB05.jpg\" height=\"80\" width=\"100\">" }, "geometry": { "type": "Point", "coordinates": [ 110.845030372919837, -7.02974579631622 ] } }
+]
+}
